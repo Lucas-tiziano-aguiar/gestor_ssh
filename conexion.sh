b458a016bd55ac7c5abf1d2e7960a78255daeb68
Lucas-tiziano-aguiar <lucasaguiarlafe@gmail.com>
2023-03-24 10:17:09 -0300

    v1.0.0

--- conexion.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh bandit0@bandit.labs.overthewire.org -p 2220
--- programa.cpp
+++ programa.cpp
@@ -1,45 +1,80 @@
-#include<iostream>
-#include<fstream>
-#include<string.h>
-#include<ctime>
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <string.h>
+#include <ctime>
+#include <lib/libssh-0.10.0/include/libssh/libssh.h>
+
 using namespace std;
+string hora();
+string direc();
+struct informacion;
+bool comprobation();
 
 int main()
 {
+	ssh_session my_session_ssh;
+	my_session_ssh = ssh_new();
+
+
+	string direccion;
 	char a;
-	string bin="#!/bin/bash",linea;
-	string direccion="bandit",nivel="0";
-	ofstream script;
 	fstream log;
-	int b=0;
-	string fecha="-";
-	do{
-	cout<<"ingrese un dato:";
-	cin>>b;
-	switch (b)
+	int b = 0;
+	do
 	{
-	case 1:
-		script.open("conexion.sh",ios::out);
-		log.open("log.txt",ios::out);
-		cout<<"ingrese a que nivel de bandit:";
-		cin>>nivel;
-		fecha=system("date");
-		direccion="ssh bandit"+nivel+"@bandit.labs.overthewire.org -p 2220";
-		cout<<"direccion:"<<direccion<<endl;
-		script << bin <<"\n"<<direccion;
-		log << time() << " " << direccion;
-		log.close();
-		script.close();
-		system("./conexion.sh");
-	break;
-	case 2:
-		log.open("log.txt",ios::in);
-		while(getline(log,linea))
+		cout << "ingrese una opcion:";
+		cin >> b;
+		
+		switch (b)
 		{
-			cout<<linea<<endl;
+		case 1:
+			direccion=direc();
+			cout << direccion;
+			break;
+		case 2:
+			break;
 		}
-		log.close();
-	break;
-	}
-	}while(b!=999);
+	} while (b != 999);
+}
+
+
+
+string hora() {
+	std::time_t result = std::time(nullptr);
+	return std::asctime(std::localtime(&result));
+}
+struct informacion
+{
+	string usuario;
+	string direccion;
+	int puerto = 22;
+};
+
+string direc(){
+	string direct = "peron";
+	informacion informacion;
+	cout << "ingresar datos\n ingrese su usuario:";
+	cin >> informacion.usuario;
+	system("clear");
+	cout << "ingresar direccion:\n"<< informacion.usuario << "@";
+	cin >> informacion.direccion;
+	do
+	{
+		cout << "ingrese puerto (No ingrese nada si desea usar el puerto determinado):";
+		cin >> informacion.puerto;
+		
+		if (!cin.good())
+		{
+			cin.ignore();	
+			cin.clear();
+		}
+
+	} while (true);
+
+	return direct;
+}
+
+bool comprobation() {
+	return true;
 }
